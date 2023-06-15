c78a7c738a61501d04644ed08956e9b1c85aca3b
nigelietti <nanaamichael@gmail.com>
2023-06-15 12:21:04 +0200

    viva la fregna yahooooooooooooo

--- src/model/MazzoG.java
+++ src/model/MazzoG.java
@@ -1,9 +1,6 @@
 package model;
 
-import java.util.ArrayList;
-
 public class MazzoG extends Mazzo {
-
     public boolean ifStuck(Seed trunf) {//metodo
         boolean trovato = false;
         for (Card k : super.getCards()) { //itera su tutte le carte(c) col metodo che eredita da mazzo
@@ -18,23 +15,48 @@ public class MazzoG extends Mazzo {
         }
 
     return false;}
+    public Value ifpoker(){
+        Value valore = null;
+        int contatore;
+        for (Value v: Value.values()){
+            contatore = 0;
+            for (Card k : super.getCards()) {
+                if (k.getValue().compareTo(v) == 0)
+                    contatore++;
 
-    public void inserisciCarta(Card card){
-        if(super.getCards().isEmpty()) {
-            super.getCards().add(card);
-            return;
-        }
-
-        for(Card c : super.getCards()){
-            if(card.getSeed().compareTo(c.getSeed()) < 0){
-                super.getCards().add(c.getSeed().ordinal(), card);
             }
+            if (contatore==4) {
+                valore=v;}
         }
+    return valore;
     }
+
     public Scala ifscala(){
+        Scala scala=new Scala();
+        Scala scalaprima = new Scala();
+        Value valore= null;
+        for (int i=0; i<10; i++)  {
+            valore=super.getCards().get(i).getValue();
+            for (int j=0; j<4; j++){
+                if (super.getCards().get(i+j+1).getSeed().compareTo(super.getCards().get(i+j).getSeed())==0 && super.getCards().get(i+j+1).getValue().compareTo(super.getCards().get(i+j).getValue())==0){
+                    scala.setNum(j+2);
+                    scala.setValue(super.getCards().get(i).getValue());
+                }
+                else{
+                    break;
+                }
+            if (scala.getNum()<3){
+                scala.setNum(0);
+                scala.setValue(null);
+                break;
+            }
+            }
+            scalaprima.setValue(scala.getValue());
+            scalaprima.setNum(scala.getNum());
+        }
 
+        return null;
     }
-
 
 
 }
